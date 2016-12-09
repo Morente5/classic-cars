ade82a94680475b286c9af0e7838470168761ba5
Alejandro Morente <morente5@hotmail.es>
2016-12-09 07:00:32 +0100

    Added search function

---
+++ scripts/search.js
@@ -0,0 +1,22 @@
+window.onload = () => {
+	var search = document.querySelector('input[name="search"]');
+	search.addEventListener('input', function() {
+		var cards = document.getElementsByClassName('card');
+		for (var i = 0; i < cards.length; i++) {
+			if (!this.value) {
+				cards[i].style.display = 'inline-block';
+			}
+			else {
+				var regExp = new RegExp(this.value,"gi");
+				var element = cards[i].querySelector('h4');
+				var content = element.textContent;
+				if (regExp.test(content)){
+					cards[i].style.display = 'inline-block';
+				}
+				else {
+					cards[i].style.display = 'none';
+				}
+			}
+		}
+	});
+};
